b45a91eb9bcefc78c80d1bf56f5f186582e89bee
no-support <tk2ektk3ek@gmail.com>
2024-03-11 20:02:08 +0900

    4.2 스타일드 컴포넌트를 사용한 스타일 구현

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     // example.com 아래의 이미지를 Image 컴포넌트에 표시하기 위해 추가한다
     domains: ["example.com"],
   },
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 module.exports = nextConfig;
