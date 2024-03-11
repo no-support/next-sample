0d370aa7216e17a5272d9ec9a27b2f577af4d2e4
no-support <tk2ektk3ek@gmail.com>
2024-03-11 17:43:00 +0900

    3.8.3 getStaticProps를 사용한 SSG를 통한 페이지 구현

--- pages/ssg.tsx
+++ pages/ssg.tsx
@@ -1,16 +1,19 @@
-import { NextPage } from 'next'
+import { GetStaticProps, NextPage } from 'next'
 // Next.js의 내장 컴포넌트
 import Head from 'next/head'
 
-type SSGProps = {}
+// 페이지 컴포넌트인 props의 타입 정의
+type SSGProps = {
+  message: string;
+}
 
-// SSG용 페이지 구현
-// NextPage는 Next.js의 Pages용 타입
-// NextPage<props>에서 props가 들어가는 Page임을 명시
-const SSG: NextPage<SSGProps> = () => {
+// SSG는 getStaticProps가 반환한 props를 받을 수 있다
+// NextPage<SSGProps>는 Message: string만을 받아 생성된 페이지 타입
+// Next.js의 페이지 컴포넌트나 함수 타입은 https://nextjs.org/learn/excel/typescript/nextjs-types도 참고한다
+const SSG: NextPage<SSGProps> = (props) => {
+  const { message } = props; 
   return (
     <div>
-      {/*  Head 컴포넌트로 감싸면, 그 요소는 <head> 태그에 배치된다 */}
       <Head>
         <title>Static Site Generation</title>
         <link rel="icon" href="/favicon.ico" />
@@ -19,9 +22,23 @@ const SSG: NextPage<SSGProps> = () => {
         <p>
           이 페이지는 정적 사이트 생성을 통해 빌드 시 생성된 페이지입니다
         </p>
+        <p>{ message }</p>
       </main>
     </div>
   )
 }
 
-export default SSG
+// getStaticProps는 빌드 시 실행된다
+// GetStaticProps<SSGProps>는 SSGProps 인수로 받는 getStaticProps 타입
+export const getStaticProps: GetStaticProps<SSGProps> = async (context) => {
+  const timestamp = new Date().toLocaleString();
+  const message = `${timestamp}에 getSaticProps가 실행되었습니다`;
+  console.log(message);
+  return {
+    props: {
+      message
+    },
+  };
+};
+
+export default SSG;
