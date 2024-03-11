a2ab127a5d3376c70825ce83e2c7e2308e3cb49f
no-support <tk2ektk3ek@gmail.com>
2024-03-11 18:38:54 +0900

    3.9.2 이미지 표시

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    // example.com 아래의 이미지를 Image 컴포넌트에 표시하기 위해 추가한다
+    domains: ["example.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
